33a181331b0a59763e9da21908d84ad773c793e2
richy7734 <richy7734.rprs@gmail.com>
2016-09-01 12:15:55 +0530

    Add files via upload
    Spring Implemented

--- uritsolution/src/main/java/com/niit/controller/AppController.java
+++ uritsolution/src/main/java/com/niit/controller/AppController.java
@@ -1,5 +1,24 @@
 package com.niit.controller;
-
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@Controller
 public class AppController {
-@controller
+String message = "Wlcome";
+@RequestMapping("/")
+public String ShowHome(){
+	System.out.println("Hello");
+		return "Home";
+	}
+@RequestMapping("/home")
+public String ShowHom(){
+	return "Home";
+	}
+@RequestMapping("/index")
+public String ShowIndex(){
+	return "index";
+	}
+@RequestMapping("/login")
+public String ShowLogin(){
+	return "login";
+	}
 }
